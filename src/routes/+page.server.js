774ec090d5ebbe8c2243fe261bdbadd361b27a44
Marco Auenhammer <marco@auenhammer.at>
2024-03-18 10:32:19 +0100

    moved to boostrap framework

---
+++ src/routes/+page.server.js
@@ -0,0 +1,14 @@
+
+
+
+export async function load() {
+    // const pb = new PocketBase('http://127.0.0.1:8090');
+
+    // const records = await pb.collection('public').getFullList({
+    //     sort: '-created',
+    // });
+
+    // return {
+    //     records
+    // };
+}
