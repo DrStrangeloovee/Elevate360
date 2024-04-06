bcdb350557fae4c4fd6c89ca8c806efa9e09d77f
Marco Auenhammer <marco@auenhammer.at>
2024-04-06 14:43:31 +0200

    turned off inspector

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,10 @@ const config = {
         // See https://kit.svelte.dev/docs/adapters for more information about adapters.
         adapter: adapter()
     },
-    preprocess: vitePreprocess()
+    preprocess: vitePreprocess(),
+    experimental: {
+        inspector: false
+    }
 };
 
 export default config;
