dfbbd54dd2aa085753f6d07df0ead907504087a1
Nick Müllner <if23b503@technikum-wien.at>
2024-04-24 14:20:25 +0200

    Implemented navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,47 +4,25 @@
     // Import all of Bootstrap's JS
 </script>
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-md bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-        >
+        <a class="navbar-brand" href="/">Elevate360</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul class="navbar-nav me-auto mb-2 mb-md-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link" href="notes">Notes</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a
-                        class="nav-link dropdown-toggle"
-                        href="#"
-                        role="button"
-                        data-bs-toggle="dropdown"
-                        aria-expanded="false"
-                    >
-                        Dropdown
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><hr class="dropdown-divider" /></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
+                    <a class="nav-link" href="calender">Calender</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                    <a class="nav-link" href="todo">To-Dos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="tracker">Tracker</a>
                 </li>
             </ul>
             <form class="d-flex" role="search">
@@ -56,6 +34,17 @@
                 />
                 <button class="btn btn-outline-success" type="submit">Search</button>
             </form>
+            <a data-mdb-dropdown-init class="nav-link dropdown-toggle" href="user" id="navbarDropdown" role="button"
+               data-mdb-toggle="dropdown" aria-expanded="false"> <i class="fas fa-user mx-1"></i> Profile </a>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                <li>
+                    <a class="dropdown-item" href="#">My account</a>
+                </li>
+
+                <li>
+                    <a class="dropdown-item" href="#">Log out</a>
+                </li>
+            </ul>
         </div>
     </div>
 </nav>
