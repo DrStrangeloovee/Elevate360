793e3bb34200ed5f9ef2305bd9fe2fb84671b130
Marco Auenhammer <marco@auenhammer.at>
2024-05-22 15:09:18 +0200

    Refined note fetching

--- src/routes/home/+page.server.js
+++ src/routes/home/+page.server.js
@@ -1,6 +1,7 @@
 export async function load({ locals }) {
     return {
         notes: await locals.pb.collection('notes').getFullList({
+            fields: 'id,title,created,updated',
             filter: `owner.id = "${locals.user.id}"` // Filter notes of current user
         })
     };
