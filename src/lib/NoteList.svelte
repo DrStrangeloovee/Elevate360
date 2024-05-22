59dcbe1b4382b560861a30f172acf08e2a1737d7
Marco Auenhammer <marco@auenhammer.at>
2024-05-22 14:49:36 +0200

    Further refinement of visuals

--- src/lib/NoteList.svelte
+++ src/lib/NoteList.svelte
@@ -8,28 +8,38 @@
         <span class="badge text-bg-secondary float-end">{notes.length}</span>
     </h5>
     <div class="card-body">
-        <a href="#" class="btn btn-primary"
+        {#if notes.length > 0}
+            <ul class="list-group list-group-flush pt-3">
+                {#each notes as note, i}
+                    <a href="#" class="list-group-item list-group-item-action">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h5>
+                                <strong>{i + 1}. </strong>
+                                {note.title}
+                            </h5>
+                        </div>
+                        <ul class="list-inline">
+                            <li class="list-inline-item">
+                                <small class="text-body-secondary"
+                                    >Last edited: {note.updated}</small
+                                >
+                            </li>
+                            <li class="list-inline-item">
+                                <small class="text-body-secondary">Created at: {note.created}</small
+                                >
+                            </li>
+                        </ul>
+                    </a>
+                {/each}
+            </ul>
+        {:else}
+            <p class="lead text-center text-body-secondary">
+                <small>You haven't created any notes yet!</small>
+            </p>
+        {/if}
+        <hr />
+        <a href="#" class="btn btn-primary float-end"
             ><i class="bi bi-file-earmark-plus border-end pe-2 me-1"></i> Create a new note
         </a>
-        <ul class="list-group list-group-flush pt-3">
-            {#each notes as note, i}
-                <a href="#" class="list-group-item list-group-item-action">
-                    <div class="d-flex w-100 justify-content-between">
-                        <h5>
-                            <strong>{i + 1}. </strong>
-                            {note.title}
-                        </h5>
-                    </div>
-                    <ul class="list-inline">
-                        <li class="list-inline-item">
-                            <small class="text-body-secondary">Last edited: {note.updated}</small>
-                        </li>
-                        <li class="list-inline-item">
-                            <small class="text-body-secondary">Created at: {note.created}</small>
-                        </li>
-                    </ul>
-                </a>
-            {/each}
-        </ul>
     </div>
 </div>
