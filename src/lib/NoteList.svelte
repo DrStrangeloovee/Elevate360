c7bc7d5c9cd723ef4d6a92a827749b3e5a88c685
Marco Auenhammer <marco@auenhammer.at>
2024-05-22 13:34:52 +0200

    Implemented note fetching & simple listing component

---
+++ src/lib/NoteList.svelte
@@ -0,0 +1,9 @@
+<script>
+    let { notes } = $props();
+</script>
+
+<ul>
+    {#each notes as note}
+        <li>{note.title}</li>
+    {/each}
+</ul>
--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,8 +1,15 @@
 <script>
     import EditorJs from '$lib/EditorJS.svelte';
+    import NoteList from '$lib/NoteList.svelte';
+    let { data } = $props();
+
+    $effect(() => {
+        console.log(data);
+    });
 </script>
 
 <div class="col-8 offset-2">
+    <NoteList notes={data.notes}></NoteList>
     <h1>Note name</h1>
-    <EditorJs></EditorJs>
+    <!--<EditorJs></EditorJs>-->
 </div>
