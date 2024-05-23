79942d42558b4219d9ab7bdc97db4ced68bab9b2
Marco Auenhammer <marco@auenhammer.at>
2024-05-23 18:53:41 +0200

    created redirect to new note

--- src/routes/note/+page.server.js
+++ src/routes/note/+page.server.js
@@ -1,11 +1,13 @@
 import { redirect } from '@sveltejs/kit';
 
 export async function load({ locals, url }) {
-    if (url.searchParams.has('id')) {
+    try {
+        // Try fetching requested note
         return {
             note: await locals.pb.collection('notes').getOne(`${url.searchParams.get('id')}`)
-        };
-    } else {
-        //redirect(307, '/note/new');
+        }
+    } catch (error) {
+        // Redirect to new empty note
+        redirect(307, '/note/new');
     }
 }
