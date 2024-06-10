a2dc1db4245a93109dc86274e23ac72a1685e74e
Nick Müllner <collina2003@scouts.at>
2024-06-10 14:17:11 +0200

    Redirecting user depending if logged in or not to "/home" or to "/"

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -9,13 +9,18 @@ export const load = ({ locals, url }) => {
     // User is logged in
     if (locals.user) {
         // Redirect to users home if logged in
-        if (url.pathname === '/') redirect(307, '/home');
+        if (url.pathname === '/' ||
+            url.pathname === '/register' ||
+            url.pathname === '/login') redirect(307, '/home');
         return {
             user: locals.user
         }
     }
 
     // User is not logged in
+    if (url.pathname !== '/register' &&
+        url.pathname !== '/login' &&
+        url.pathname !== '/') redirect(307, '/');
     return {
         user: undefined
     }
