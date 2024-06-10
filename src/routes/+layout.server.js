da1580d0891d53e51cdf5b57b1ac7f69775a15d0
Nick Müllner <collina2003@scouts.at>
2024-06-10 18:33:28 +0200

    No redirection to "/home" when logged in and trying to call "/" (because you may want to visit the main page even if you are logged in)

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -9,8 +9,7 @@ export const load = async ({ locals, url }) => {
     // User is logged in
     if (locals.user) {
         // Redirect to users home if logged in
-        if (url.pathname === '/' ||
-            url.pathname === '/register' ||
+        if (url.pathname === '/register' ||
             url.pathname === '/login') redirect(307, '/home');
         locals.user.avatarURL = locals.pb.files.getUrl(locals.user, locals.user.avatar);
         return {
