0a58dc56666df5527b6e7a11fb99f4bf8b179621
Nick Müllner <collina2003@scouts.at>
2024-06-10 16:07:35 +0200

    Implemented profile pic Added icons Fixed broken links Fixed to wide navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,15 @@
     let { data } = $props();
 </script>
 
-<div class="row mb-5 bg-white" class:sticky-top={!data.user}>
+<style>
+    .profile-pic {
+        object-fit: cover;
+        width: 32px;
+        height: 32px;
+    }
+</style>
+
+<div class="mb-5 bg-white" class:sticky-top={!data.user}>
     <header
         class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between border-bottom px-2"
     >
@@ -23,13 +31,13 @@
                     </a>
                 </li>
                 <li class="nav-item px-2">
-                    <a class="nav-link" href="/notes">
+                    <a class="nav-link" href="/note">
                         <i class="bi bi-list-nested d-block mx-auto"></i>
                         Notes
                     </a>
                 </li>
                 <li class="nav-item px-2">
-                    <a class="nav-link" href="/calender">
+                    <a class="nav-link" href="/calendar">
                         <i class="bi bi-calendar d-block mx-auto"></i>
                         Calendar
                     </a>
@@ -49,14 +57,27 @@
             </ul>
             <div class="col-md-3 text-end">
                 <form action="/logout" method="POST">
+                    <a class="me-2" href="/profile">
+                        {#if data.user.img}
+                            <img class="profile-pic rounded-circle" src="{data.user.img}" alt="profile"/>
+                        {:else}
+                            <i style="vertical-align: middle;" class="bi bi-person-circle h2"></i>
+                        {/if}
+                    </a>
                     <button type="submit" class="btn btn-danger">Logout</button>
                 </form>
             </div>
         {:else}
             <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0 text-center">
-                <li><a href="/" class="nav-link px-2">Start</a></li>
-                <li><a href="/#features" class="nav-link px-2">Features</a></li>
-                <li><a href="/#about" class="nav-link px-2">About</a></li>
+                <li><a href="/" class="nav-link px-2">
+                    <i class="bi bi-house d-block mx-auto"></i>Start</a>
+                </li>
+                <li><a href="/#features" class="nav-link px-2">
+                    <i class="bi bi-code-square d-block mx-auto"></i>Features</a>
+                </li>
+                <li><a href="/#about" class="nav-link px-2">
+                    <i class="bi bi-info-circle d-block mx-auto"></i>About</a>
+                </li>
             </ul>
             <div class="col-md-3 text-end">
                 <a href="/login" class="btn btn-primary m-2">Login </a>
@@ -66,6 +87,6 @@
     </header>
 </div>
 
-<div class="row">
+<div>
     <slot />
 </div>
